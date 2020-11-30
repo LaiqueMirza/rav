ecd02113c65a8114f67d11a486f59f48f3e2187a
LaiqueMirza <mirzalaique2ey@gmail.com>
2020-11-30 22:27:07 +0530

    Updates

---
+++ precache-manifest.d1b54607b979939d4ee8ed6f2aa3eae4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beadf4f9d46e2366fb17b49790c66c96",
+    "url": "/rav.git/index.html"
+  },
+  {
+    "revision": "339e8dc20931a447b6c1",
+    "url": "/rav.git/static/css/main.91a102e2.chunk.css"
+  },
+  {
+    "revision": "40a536335e184874c60b",
+    "url": "/rav.git/static/js/2.ff10b991.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rav.git/static/js/2.ff10b991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339e8dc20931a447b6c1",
+    "url": "/rav.git/static/js/main.42361c54.chunk.js"
+  },
+  {
+    "revision": "af530a97df4d361c0047",
+    "url": "/rav.git/static/js/runtime-main.5c4ec5a8.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/rav.git/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/rav.git/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
